ba082740fbc7aa3655c0b40c6ee6cf79c0f5b49e
Ahrar <ahrar@deriv.com>
2023-10-09 15:21:19 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/ahrar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ahrar.maestro_bitrise_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:maestro_bitrise_flutter/app/app.dart';
+import 'package:maestro_bitrise_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
